3bd2a1d201e20d7fe9daf5f6efadc49b9c328651
ninjapigeon <ninjapigeon@me.com>
2014-04-13 09:58:53 -0700

    Now with the real stuff

--- Real code/main.cpp
+++ Real code/main.cpp
@@ -6,13 +6,20 @@
 //  Copyright (c) 2014 Darth Sith. All rights reserved.
 //
 
+#include <ctime>
+#include <cstdlib>
 #include <iostream>
-
-int main(int argc, const char * argv[])
+using namespace std;
+int randRange (int low, int high)
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    // we get a random number, get it to be between 0 and the number // of values in our range, then add the lowest possible value return rand() % ( high - low + 1 ) + low;
 }
 
+int main ()
+{
+    srand( time( NULL ) );
+    for ( int i = 0; i < 1000; ++i )
+    {
+        cout << randRange( 4, 10 ) << '\n';
+    }
+}
