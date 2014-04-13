ef0c00a397b689161fd7905aca8ce411e1e4ab40
ninjapigeon <ninjapigeon@me.com>
2014-04-13 09:56:12 -0700

    Initial Commit

---
+++ Real code/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Real code
+//
+//  Created by Darth Sith on 2014-04-13.
+//  Copyright (c) 2014 Darth Sith. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
