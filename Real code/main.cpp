0919e07cf37bc5c1413de3849815e58eabddaa74
ninjapigeon <ninjapigeon@me.com>
2014-04-18 11:23:49 -0700

    correcting time to time_t

--- Real code/main.cpp
+++ Real code/main.cpp
@@ -12,12 +12,14 @@
 using namespace std;
 int randRange (int low, int high)
 {
-    // we get a random number, get it to be between 0 and the number // of values in our range, then add the lowest possible value return rand() % ( high - low + 1 ) + low;
+    // we get a random number, get it to be between 0 and the number
+    // of values in our range, then add the lowest possible value
+    return rand() % ( high - low + 1 ) + low;
 }
 
 int main ()
 {
-    srand( time( NULL ) );
+    srand( time_t( NULL ) );
     for ( int i = 0; i < 1000; ++i )
     {
         cout << randRange( 4, 10 ) << '\n';
