e0a389244a1f7ebe0e0fc11221b392bc7f8ea9f5
ninjapigeon <ninjapigeon@me.com>
2014-04-26 12:17:27 -0700

    Broken coin flip… Coin always results in “0” and breaks thread… more to check later.

--- Real code/main.cpp
+++ Real code/main.cpp
@@ -14,14 +14,24 @@ int randRange (int low, int high)
 {
     // we get a random number, get it to be between 0 and the number
     // of values in our range, then add the lowest possible value
-    return rand() % ( high - low + 1 ) + low;
+    return rand() % ( high - low +1) + low;
 }
+
 
 int main ()
 {
+    int Coin = 0;
     srand( time_t( NULL ) );
     for ( int i = 0; i < 100; ++i )
     {
-        cout << randRange( 1, 2 ) << '\n';
+        Coin = randRange( 4, 10 );
+        switch (Coin)
+        {
+            case 2 : cout << "Heads \n";
+                break;
+            case 1 : cout << "Tails \n";
+                break;
+            default: cout << "Error \n";
+        }
     }
 }
