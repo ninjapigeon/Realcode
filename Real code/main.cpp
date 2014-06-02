3417a6b41c639b68081efb27a925a9d73aa15d26
ninjapigeon <ninjapigeon@me.com>
2014-06-02 16:17:50 -0700

    Updating to most recent files (last were 2 months old)

--- Real code/main.cpp
+++ Real code/main.cpp
@@ -10,18 +10,29 @@
 #include <cstdlib>
 #include <iostream>
 using namespace std;
+
 int randRange (int low, int high)
 {
     // we get a random number, get it to be between 0 and the number
     // of values in our range, then add the lowest possible value
-    return rand() % ( high - low + 1 ) + low;
+    return rand() % ( high - low +1) + low;
 }
+
 
 int main ()
 {
     srand( time_t( NULL ) );
-    for ( int i = 0; i < 1000; ++i )
+    for ( int i = 0; i < 100; ++i )
     {
-        cout << randRange( 4, 10 ) << '\n';
+        
+        switch (randRange( 1, 2 )) // At this point, i'm merely trying to output the appropriate text based on the result.
+        {
+            case 2 : cout << "Heads \n";
+                break;
+            case 1 : cout << "Tails \n";
+                break;
+            default: cout << "Error \n";
+                break;
+        }
     }
 }
